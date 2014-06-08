96b1bc7e132a6c8cbabb753166390036668fce8a
task650 <task650@gmail.com>
2014-06-07 19:00:00 -0700

    Revert "Add HD Audio capability"
    This reverts commit 9a82c45e4fe879c75dc0704191c35b87b21e32db.

--- sound/soc/msm/qdsp6v2/q6adm.c
+++ sound/soc/msm/qdsp6v2/q6adm.c
@@ -25,10 +25,6 @@
 #include <sound/q6afe-v2.h>
 
 #include "audio_acdb.h"
-#undef pr_info
-#undef pr_err
-#define pr_info(fmt, ...) pr_aud_info(fmt, ##__VA_ARGS__)
-#define pr_err(fmt, ...) pr_aud_err(fmt, ##__VA_ARGS__)
 
 #define TIMEOUT_MS 1000
 
@@ -38,11 +34,6 @@
 
 #define ULL_SUPPORTED_SAMPLE_RATE 48000
 #define ULL_MAX_SUPPORTED_CHANNEL 2
-
-#ifdef CONFIG_HD_AUDIO
-#define ULL_SUPPORTED_BITS_PER_SAMPLE 16
-#endif
-
 enum {
 	ADM_RX_AUDPROC_CAL,
 	ADM_TX_AUDPROC_CAL,
@@ -1139,10 +1130,8 @@ int adm_open(int port_id, int path, int rate, int channel_mode, int topology,
 	int index;
 	int tmp_port = q6audio_get_port_id(port_id);
 
-	pr_info("%s: port %#x path:%d rate:%d mode:%d perf_mode:%d,\
-			topology: %d, bits_per_sample: %d\n", __func__,
-			port_id, path, rate, channel_mode, perf_mode,
-			topology, bits_per_sample);
+	pr_info("%s: port %#x path:%d rate:%d mode:%d perf_mode:%d\n",
+		 __func__, port_id, path, rate, channel_mode, perf_mode);
 
 	port_id = q6audio_convert_virtual_to_portid(port_id);
 
@@ -1218,9 +1207,6 @@ int adm_open(int port_id, int path, int rate, int channel_mode, int topology,
 			rate = ULL_SUPPORTED_SAMPLE_RATE;
 			if(channel_mode > ULL_MAX_SUPPORTED_CHANNEL)
 				channel_mode = ULL_MAX_SUPPORTED_CHANNEL;
-#ifdef CONFIG_HD_AUDIO
-			bits_per_sample = ULL_SUPPORTED_BITS_PER_SAMPLE;
-#endif
 		}
 		open.dev_num_channel = channel_mode & 0x00FF;
 		open.bit_width = bits_per_sample;
