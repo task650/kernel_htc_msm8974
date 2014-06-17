7f6dc6490574598ea53071abb21bbafffeae8429
Paul Reioux <reioux@gmail.com>
2014-06-16 19:51:50 -0500

    intellidemand: re-introduce intellidemand 5.5 cpu governor
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -315,7 +315,10 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDPLUS)
 extern struct cpufreq_governor cpufreq_gov_ondemandplus;
-#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandplus)
+#define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_ondemandplus)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTELLIDEMAND)
+extern struct cpufreq_governor cpufreq_gov_intellidemand;
+#define CPUFREQ_DEFAULT_GOVERNOR    (&cpufreq_gov_intellidemand)
 #endif
 
 
